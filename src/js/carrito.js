ba4f955c4e016b4b01ead3c0ea5beda7c72e5f4f
mirthaLopez <melopez@fwdcostarica.com>
2024-08-12 00:21:55 -0600

    carrito

--- src/js/carrito.js
+++ src/js/carrito.js
@@ -1,4 +1,7 @@
 const contenedorProductos = document.getElementById("contenedorProductos");
+const totalArt=document. getElementById("totalArt");
+const totalPago=document. getElementById("totalPago");
+
 
 let listChart = JSON.parse(localStorage.getItem("listaCarrito")) || [];
 let total = 0;
@@ -12,7 +15,7 @@ for (let index = 0; index < listChart.length; index++) {
     totalArticulos = totalArticulos + listChart[index].quantity;
     let producto = document.createElement("div")
     contenedorProductos.appendChild(producto);
-    producto.className = "contenedorArticulo"
+    producto.className = "contenedorArticulo";
 
     /// creamos etiquetas h3 donde se almacena cada articulo
     let articulo = document.createElement("h3")
@@ -21,17 +24,17 @@ for (let index = 0; index < listChart.length; index++) {
     articulo.innerHTML = "Articulo: " + listChart[index].productName; 
 
     let precio=document.createElement("h5");
-    articulo.appendChild(precio); 
-    precio.innerHTML="Precio por unidad: "+listChart[index].price;
+    producto.appendChild(precio); 
+    precio.innerHTML="Precio por unidad: $"+listChart[index].price;
 
     let cantidad=document.createElement("h5");
-    articulo.appendChild(cantidad); 
+    producto.appendChild(cantidad); 
     cantidad.innerHTML="Cantidad de articulos ordenados: "+listChart[index].quantity;
     /// Crea un boton Eliminar
     let btnEliminar = document.createElement("button") 
     btnEliminar.innerHTML ="Eliminar";
     btnEliminar.className = "btnEliminar"
-    articulo.appendChild(btnEliminar); 
+    producto.appendChild(btnEliminar); 
     
     btnEliminar.addEventListener("click", function () {
         let articulos = JSON.parse(localStorage.getItem("listaCarrito")) || [] 
@@ -43,5 +46,7 @@ for (let index = 0; index < listChart.length; index++) {
 
 }
 
+totalArt.innerHTML="La cantidad total de articulos a pagar es: "+totalArticulos;
+totalPago.innerHTML="El monto total a cancelar es: $"+total;
 console.log(total);
 console.log(totalArticulos);
